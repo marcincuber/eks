29821fa91fc1ceb1358cd30d101fbc664dd7a598
Marcin Cuber <marcincuber@hotmail.com>
2020-11-17 10:00:23 +0000

    Update versions.tf

--- terraform-aws/versions.tf
+++ terraform-aws/versions.tf
@@ -1,12 +1,26 @@
 #####
 # Backend and provider config
 #####
-
 terraform {
-  required_version = ">= 0.13.1"
+  required_version = ">= 0.13.5"
 
   backend "remote" {
     hostname = "app.terraform.io"
+  }
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.5"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 2.3"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 2.2"
+    }
   }
 }
 
@@ -15,7 +29,6 @@ provider "aws" {
     role_arn     = var.aws_role_arn
     session_name = "EKS_deployment_session_${var.tags["Environment"]}"
   }
-
-  version = ">= 3.5"
-  region  = var.region
+  
+  region = var.region
 }
