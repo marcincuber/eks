77b9fc35dc67b4ad4936acb5c5d7d839f146f32f
Marcin Cuber <marcincuber@hotmail.com>
2020-12-02 11:49:52 +0000

    Update versions.tf

--- terraform-aws/versions.tf
+++ terraform-aws/versions.tf
@@ -11,7 +11,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.5"
+      version = ">= 3.19"
     }
     random = {
       source  = "hashicorp/random"
