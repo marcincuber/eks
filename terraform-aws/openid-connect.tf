a857ede98b7297eccc607e10c036412631cf8cc6
Marcin Cuber <marcincuber@hotmail.com>
2021-02-16 08:48:57 +0000

    Update openid-connect.tf

--- terraform-aws/openid-connect.tf
+++ terraform-aws/openid-connect.tf
@@ -2,7 +2,7 @@ resource "aws_iam_openid_connect_provider" "cluster" {
   count = var.create_cluster ? 1 : 0
 
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = concat([data.external.thumbprint.result.thumbprint], var.oidc_thumbprint_list)
+  thumbprint_list = concat([data.tls_certificate.cluster[0].certificates.0.sha1_fingerprint], var.oidc_thumbprint_list)
   url             = aws_eks_cluster.cluster[0].identity.0.oidc.0.issuer
 }
 
