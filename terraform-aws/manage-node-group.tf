038ecb5397f96751ca8a3d3aeadffc62b0e385e6
Marcin Cuber <marcincuber@hotmail.com>
2022-04-05 08:06:41 +0100

    Update manage-node-group.tf

--- terraform-aws/manage-node-group.tf
+++ terraform-aws/manage-node-group.tf
@@ -1,6 +1,6 @@
 module "eks-node-group-a" {
-  source  = "umotif-public/eks-node-group/aws"
-  version = "~> 4.0.0"
+  source = "native-cube/eks-node-group/aws"
+  version = "~> 1.0.0"
 
   count           = var.enable_managed_workers ? 1 : 0
   create_iam_role = false
@@ -28,9 +28,9 @@ module "eks-node-group-a" {
 }
 
 module "eks-node-group-b" {
-  source  = "umotif-public/eks-node-group/aws"
-  version = "~> 4.0.0"
-
+  source = "native-cube/eks-node-group/aws"
+  version = "~> 1.0.0"
+  
   count           = var.enable_managed_workers ? 1 : 0
   create_iam_role = false
 
