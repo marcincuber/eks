b6c10adba5814eb7711c9a347cd4ae9fcb61bf32
Marcin Cuber <marcincuber@hotmail.com>
2022-04-05 08:09:44 +0100

    Update vpc.tf

--- terraform-aws/vpc.tf
+++ terraform-aws/vpc.tf
@@ -16,7 +16,7 @@ resource "aws_default_security_group" "default" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.1.0"
+  version = "3.14.0"
 
   name = "${local.name_prefix}-vpc"
 
@@ -145,8 +145,8 @@ resource "aws_security_group_rule" "vpc_endpoint_self_ingress" {
 #####
 
 module "vpc-flow-logs" {
-  source  = "umotif-public/vpc-flow-logs/aws"
-  version = "~> 1.1.2"
+  source = "native-cube/vpc-flow-logs/aws"
+  version = "~> 1.0.0"
 
   name_prefix = local.name_prefix
   vpc_id      = module.vpc.vpc_id
