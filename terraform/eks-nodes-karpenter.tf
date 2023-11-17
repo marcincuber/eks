a9bee6d9cd4a8a236006d4017ec337af2fd99ebb
Marcin Cuber <5611060+marcincuber@users.noreply.github.com>
2023-11-17 08:45:42 +0000

    add cloudwatch managed policy to karpenter nodes

--- terraform/eks-nodes-karpenter.tf
+++ terraform/eks-nodes-karpenter.tf
@@ -10,7 +10,8 @@ resource "aws_iam_role" "eks_node_karpenter" {
     "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
     "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
+    "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
   ]
 }
 
