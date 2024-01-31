65191a47108441c5339818f58592f86d9d122c1e
Marcin Cuber <5611060+marcincuber@users.noreply.github.com>
2024-01-31 11:18:27 +0000

    Update eks-cluster.tf

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -26,7 +26,7 @@ resource "aws_eks_cluster" "cluster" {
 
   access_config {
     authentication_mode                         = "API_AND_CONFIG_MAP"
-    bootstrap_cluster_creator_admin_permissions = true
+    bootstrap_cluster_creator_admin_permissions = false
   }
 
   kubernetes_network_config {
