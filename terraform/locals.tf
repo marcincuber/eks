30cbd7992f5a8d6fff910b9fb9d3312895c28879
Marcin Cuber <5611060+marcincuber@users.noreply.github.com>
2024-05-01 10:57:01 +0100

    remove legacy service ipv4 local

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,6 +1,4 @@
 locals {
-  cluster_service_cidr = cidrhost(var.eks_service_ipv4_cidr, 10) # set to X.X.X.10 for CoreDNS service
-
   events = {
     health_event = {
       name        = "HealthEvent"
