c51fc275c612f65d6e163508e13b9915e8e5cb51
Marcin Cuber <5611060+marcincuber@users.noreply.github.com>
2024-09-20 10:32:48 +0100

    Move eks configuration to API auth mode only

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -25,7 +25,7 @@ resource "aws_eks_cluster" "cluster" {
   }
 
   access_config {
-    authentication_mode                         = "API_AND_CONFIG_MAP"
+    authentication_mode                         = "API"
     bootstrap_cluster_creator_admin_permissions = false
   }
 
