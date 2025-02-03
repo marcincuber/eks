9efecc36f0e95f62a7e3e1a2776640dba839fe8e
Marcin Cuber <5611060+marcincuber@users.noreply.github.com>
2025-02-03 12:35:13 +0000

    Update EKS cluster IAM Role perms
    Adding policies to support EKS auto-mode

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -55,6 +55,10 @@ resource "aws_iam_role_policy_attachments_exclusive" "cluster" {
   role_name = aws_iam_role.cluster.name
   policy_arns = [
     "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSComputePolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSBlockStoragePolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSNetworkingPolicy",
     "arn:aws:iam::aws:policy/AmazonEKSServicePolicy",
     "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   ]
