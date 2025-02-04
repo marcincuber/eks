07438801cbe90cbf83697a67322c91e1dc1b111a
Marcin Cuber <5611060+marcincuber@users.noreply.github.com>
2025-02-04 06:38:28 +0000

    Update cluster role trusted policy
    add "sts:TagSession" permission to cluster role

--- terraform/data.tf
+++ terraform/data.tf
@@ -19,7 +19,7 @@ data "tls_certificate" "cluster" {
 
 data "aws_iam_policy_document" "cluster_role_assume_role_policy" {
   statement {
-    actions = ["sts:AssumeRole"]
+    actions = ["sts:AssumeRole", "sts:TagSession"]
 
     principals {
       type        = "Service"
